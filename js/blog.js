9d14982ed43cf347e151b6cb19a9812d166daa8d
drisaden <drisaden@gmail.com>
2024-02-06 14:46:53 +0100

    update

--- js/blog.js
+++ js/blog.js
@@ -2,16 +2,12 @@ window.onload = function () {
 fetch('https://www.googleapis.com/blogger/v3/blogs/7706273476706534553/posts?key=AIzaSyD5dD55I9lEVPKAa5Gr_HcnpZIkhqdpXj0')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+     //   console.log(data);
        // const recentItems = data.items.filter(item => item.labels.includes('Recent'));
         
 
         const populateBlog = document.getElementById('populateBlog');
-         // <a href="https://adenaikeidris.com.ng/single.html?id=${item.id}" url
-       //   ${item.title} title
-       //${item.published.split('T')[0]} date
-       //const src = item.content.match(/<img[^>]*src="([^"]*)"/)?.[1]; image
-     //  ${item.content.replace(/<[^>]+>/g, '').split(' ').slice(0, 20).join(' ') content
+   
 
         data.items.forEach(item => {
             const src = item.content.match(/<img[^>]*src="([^"]*)"/)?.[1];
@@ -26,14 +22,11 @@ fetch('https://www.googleapis.com/blogger/v3/blogs/7706273476706534553/posts?key
                       <h3 class="text-base md:text-2xl font-bold tracking-tight uppercase"> ${item.title} </h3>
                        <p class="text-sm md:text-lg font-semibold my-6 text-gray-600"> <i class="fas fa-calendar"></i> ${item.published.split('T')[0]} </p>
                      <p class="text-sm mb-8 md:text-2xl tracking-tight "> ${item.content.replace(/<[^>]+>/g, '').split(' ').slice(0, 20).join(' ')} ... </p>
-                     <a href="https://muhibqosim.github.io/buk-law-clinic/single.html?id=${item.id}" class=""> <p class="text-sm button py-3 md:text-2xl bg-red-700 text-white  mb-5 font-semibold">Read Full » </p> </a> 
+                     <a href="https://buklawclinic.com.ng/single.html?id=${item.id}" class=""> <p class="text-sm button py-3 md:text-2xl bg-red-700 text-white  mb-5 font-semibold">Read Full » </p> </a> 
                     </div>
-                </div>
-            `
+                </div>`;
 });
-        
-
-    })
+})
     .catch(error => {
         console.log('Error fetching data:', error);
     });
